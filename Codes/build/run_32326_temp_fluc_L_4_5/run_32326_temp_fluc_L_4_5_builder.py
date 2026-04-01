28aef4fc2d7165f3a9d3a9a26817e9ff1343a782
Sina Rezvani <sxr190099@g2-l-01.localdomain>
2026-04-01 14:45:30 -0500

    Commit before run run_32326_temp_fluc_L_4_5

--- Codes/build/run_32326_temp_fluc_L_4_5/run_32326_temp_fluc_L_4_5_builder.py
+++ Codes/build/run_32326_temp_fluc_L_4_5/run_32326_temp_fluc_L_4_5_builder.py
@@ -41,7 +41,7 @@ template_contents=open(template_file,'r').read()
 
 vnum=0
 
-for L in range(100):
+for L in range(40):
         qsub_file=template_file.replace('.template','_'+str(vnum)+'.qsub')
         fout=open(qsub_file,'w')
 
